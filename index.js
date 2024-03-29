680471f8a4544a83028281b1bfed4aa7b6175593
Chakshu Gautam <chaks.gautam@gmail.com>
2024-03-29 18:27:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const { io } = require("socket.io-client");
+
+baseUrl = "https://ts.staging.bhasai.samagra.io";
+
+const connectionOptions = {
+    transportOptions: {
+        polling: {
+            extraHeaders: {
+                // Authorization: `Bearer ${localStorage.getItem('auth')}`,
+                channel: 'akai',
+            },
+        },
+    },
+    query: {
+        deviceId: "59bb1c5d-4cb9-43a9-b12a-403acdc79ea2",
+    },
+    autoConnect: false,
+    transports: ['polling', 'websocket'],
+    upgrade: true,
+};
+
+const socket = io(baseUrl, connectionOptions);
+const queryFromUser = "How to grow rice?";
+
+socket.on("connect", async () => {
+    console.log("connecting");
+    setTimeout(() => {
+        console.log("Sending request");
+        socket.emit("botRequest", {
+            content: {
+                text: queryFromUser,
+                to: "59bb1c5d-4cb9-43a9-b12a-403acdc79ea2",
+                from: "7398050181",
+                appId: 'AKAI_App_Id',
+                channel: 'AKAI',
+                latitude: "26.7821706",
+                longitude: "79.034109",
+                city: "New Delhi",
+                state: "National Capital Territory of Delhi",
+                ip: "106.215.88.94",
+                asrId: null,
+                userId: "59bb1c5d-4cb9-43a9-b12a-403acdc79ea2",
+                conversationId: "faa94cf9-d8ee-4aeb-8312-5e6f1249307c",
+                botId: "0477e54a-09f5-4cc6-a12c-e41d298d7342"
+            }
+        });
+    }, 1000);
+});
+
+socket.on("session", (session) => {
+    console.log("connected");
+    console.log({ session });
+});
+
+socket.on("botResponse", async (data) => {
+    console.log(data.payload.text)
+    if (data.payload.text.includes("<end/>"))
+        socket.close();
+});
+
+socket.connect();
